6f96dccbf3c04e37b12bc2902b9ade494ef72fae
Mohamed <mohamed3773@gmail.com>
2026-01-29 20:29:55 +0300

    Enable static export for Netlify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
+    images: { unoptimized: true },
+
     webpack: (config) => {
         config.externals.push("pino-pretty", "lokijs", "encoding");
         return config;
